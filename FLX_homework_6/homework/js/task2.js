71ec13d2d42b4f894aff4e0081bc7604ad1465f7
Roman Kuk <34649365+RomanCook@users.noreply.github.com>
2019-02-05 19:52:25 +0200

    Update task2.js

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -5,7 +5,7 @@ discount = prompt('Enter amount of discount in range 0 to 99');
 
 if (money < 0 || discount < 0 || money > 9999999 || discount > 99 || money === null || discount === null || isNaN(money)
     || isNaN(discount)) {
-    alert(`For input data ${money}(price) and ${discount}(discount):\n      'Invalid data'`);
+    alert(`For input data ${money}(price) and ${discount}(discount):\n      'Invalid input data'`);
 } else {
     price_discount = money - money * discount / 100;
     saved = money - price_discount;
